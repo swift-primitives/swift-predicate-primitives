44eac724305700e0e67871b4b75ff1dff4375f1d
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-11 19:04:50 +0100

    Rename test dependency to swift-test-primitives

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../swift-ternary-logic-primitives"),
-        .package(path: "../swift-test-support-primitives"),
+        .package(path: "../swift-test-primitives"),
     ],
     targets: [
         .target(
@@ -32,7 +32,7 @@ let package = Package(
             name: "Predicate Primitives Tests",
             dependencies: [
                 "Predicate Primitives",
-                .product(name: "Test Support Primitives", package: "swift-test-support-primitives"),
+                .product(name: "Test Primitives", package: "swift-test-primitives"),
             ]
         ),
     ],
